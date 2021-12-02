b23e7e6592cf780fd6651cba11bc57e3644629ad
Mikkel Folting <folti002@umn.edu>
2021-12-02 10:18:17 -0600

    More curly brace fixes

--- src/utils.c
+++ src/utils.c
@@ -1,29 +1,24 @@
 #include "utils.h"
 
-void _removeOutputDir()
-{
+void _removeOutputDir() {
     pid_t pid = fork();
-    if(pid == 0)
-    {
+    if(pid == 0){
         char *argv[] = {"rm", "-rf", "output", NULL};
-        if (execvp(*argv, argv) < 0)
-        {
+        if (execvp(*argv, argv) < 0){
             printf("ERROR: exec failed\n");
             exit(EXIT_FAILURE);
         }
         exit(EXIT_SUCCESS);
-    } else{
+    } else {
         wait(NULL);
     }
 }
 
-void _createOutputDir()
-{
+void _createOutputDir(){
     mkdir("output", ACCESSPERMS);
 }
 
-void bookeepingCode()
-{
+void bookeepingCode(){
     _removeOutputDir();
     sleep(1);
     _createOutputDir();
