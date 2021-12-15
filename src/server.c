be39299873175599b670aba48035db5b7d4b76af
Mikkel Folting <folti002@umn.edu>
2021-12-15 10:29:54 -0600

    registering an account should work now

--- src/server.c
+++ src/server.c
@@ -14,18 +14,96 @@ HISTORY (11)
 int balancesSize = 0;
 pthread_mutex_t balancesSizeLock = PTHREAD_MUTEX_INITIALIZER;
 
+// Logic for registering an account
+void registerAccount(int connfd){
+    if(balancesSize == MAX_ACC){
+        printf("%s ERROR: max account number reached", serverStr);
+        return;
+    }
+    int amt = 0;
+
+    // Set up variables to be read in from socket
+    char username[MAX_STR];
+    memset(username, 0, MAX_STR);
+    char name[MAX_STR];
+    memset(name, 0, MAX_STR);
+    time_t birthday;
+
+    // Get username for new account
+    if((amt=read(sockfd, username, sizeof(char)*MAX_STR)) < 0){
+        printf("%s registerAccount failed to read username\n.", serverStr);
+        printf("It read %d bytes\n.", amt);
+        exit(1);
+    }
+    // Get name for new account
+    if((amt=read(sockfd, name, sizeof(char)*MAX_STR)) < 0){
+        printf("%s registerAccount failed to read name\n.", serverStr);
+        printf("It read %d bytes\n.", amt);
+        exit(1);
+    }
+    // Get birthday for new account
+    if((amt=read(sockfd, &birthday, sizeof(time_t)) != sizeof(time_t)){
+        printf("%s registerAccount failed to read birthday\n.", serverStr);
+        printf("It read %d bytes\n.", amt);
+        exit(1);
+    }
+
+    // Lock the shared balance structures
+    pthread_mutex_lock(&balancesSizeLock);
+    pthread_mutex_lock(&balances[balancesSize].lock);
+
+    // Register a new account
+    strcpy(balances[balancesSize].username, username);
+    strcpy(balances[balancesSize].name, name);
+    balances[balancesSize].birthday = birthday;
+    balances[balancesSize].balance = 0.0;
+    balances[balancesSize].accountNumber = balancesSize;
+
+    // Return BALANCE message to client
+    int msg = BALANCE;
+
+    // Write message type
+    if((amt=write(sockfd, &msg, sizeof(int))) != sizeof(int)){
+        printf("%s registerAccount failed to write msg\n.", serverStr);
+        printf("It wrote %d bytes\n.", amt);
+        exit(1);
+    }
+    // Write account number
+    if((amt=write(sockfd, &balancesSize, sizeof(int))) != sizeof(int)){
+        printf("%s registerAccount failed to write accountNumber\n.", serverStr);
+        printf("It wrote %d bytes\n.", amt);
+        exit(1);
+    }
+    // Write balance for this customer (will be 0.0 because this account was just inititalized)
+    if((amt=write(sockfd, &(balances[balancesSize].balance), sizeof(float))) != sizeof(float)){
+        printf("%s registerAccount failed to write balance\n.", serverStr);
+        printf("It wrote %d bytes\n.", amt);
+        exit(1);
+    }
+
+    // Increment size of balances structure and unlock shared data
+    pthread_mutex_unlock(&balances[balancesSize].lock);
+    balancesSize++;
+    pthread_mutex_unlock(&balancesSizeLock);
+}
+
 // Logic for sending ACCOUNT_INFO
-void account_info(int connfd, char username[MAX_STR], char name[MAX_STR]){
+void accountInfo(int connfd){
     printf("%s Account_info protocol\n", serverStr);
 }
 
+// Logic for handling a TRANSACT request
+void transact(int connfd){
+    printf("%s Transfer protocol\n", serverStr);
+}
+
 // Logic for sending BALANCE
-void balance(int connfd, int accountNumber, float balance){
+void balanceHandler(int connfd){
     printf("%s Balance protocol\n", serverStr);
 }
 
 // Logic for sending CASH
-void cash(int connfd, float cash){
+void cash(int connfd){
     printf("%s Cash protocol\n", serverStr);
 }
 
@@ -66,6 +144,7 @@ void* requestHandler(void* connfd){
     int msg;
     int connectionfd = *((int*) connfd);
 
+    // Keep reading from client until we encounter a TERMINATE message
     while(1){
         if((size = read(*connectionfd, &msg, sizeof(int))) != sizeof(int)){
             printf("%s failed to read message type from client\n", serverStr);
@@ -74,24 +153,25 @@ void* requestHandler(void* connfd){
         }
 
         if(msg == REGISTER){
-            
+            registerAccount(connectionfd);
         } else if(msg == GET_ACCOUNT_INFO){
-
+            accountInfo(connectionfd);
         } else if(msg == TRANSACT){
-
+            transact(connectionfd);
         } else if(msg == GET_BALANCE){
-
+            balanceHandler(connectionfd);
         } else if(msg == REQUEST_CASH){
-
+            cash(connectionfd);
         } else if(msg == ERROR){    
 
         } else if(msg == TERMINATE){
-            close(connfd);
+            close(connnectionfd);
             break;
         } else if(msg == REQUEST_HISTORY){
 
         } else {
             printf("%s Invalid message type read from client!\n", serverStr);
+            exit(1);
         }
     }
     return NULL;
@@ -111,6 +191,9 @@ int main(int argc, char *argv[]){
     char* IPaddress = argv[1];
     int port = atoi(argv[2]);
     int numWorkers = atoi(argv[3]);
+
+    // Set up the global balances structure
+    balances = (account*) malloc(sizeof(account) * MAX_ACC);
 
     // Launch log thread
     pthread_t tid;
@@ -166,7 +249,9 @@ int main(int argc, char *argv[]){
     pthread_t pid;
     int connfd;
 
+    // Continue accepting clients forever until launcher kills server.c
     while(1){
+        // Accept an incoming request
         int* connfd = malloc(sizeof(int));
         *connfd = accept(sockfd, (SA*) &cli, &len);
         if(*connfd < 0){
@@ -176,6 +261,7 @@ int main(int argc, char *argv[]){
             printf("%s Server has accepted the client!\n", serverStr);
         }
 
+        // Spawn a worker thread and detach it to handle the requests of the client
         pthread_t worker_tid;
         if(pthread_create(&worker_tid, NULL, requestHandler, (void*) connfd) != 0){
             printf("%s Worker thread failed to create\n", serverStr);
@@ -184,6 +270,9 @@ int main(int argc, char *argv[]){
         pthread_detach(worker_tid);
     }
 
+    // Clean up (this will actually never happen since the above while loop never breaks)
+    free(balances);
+    free(connfd);
     close(sockfd);
 
     return 0; 
